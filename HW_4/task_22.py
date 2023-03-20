c809b25a2f9a03a025eb912a42656b098af8d290
Liliya <105841191+Bayliliya@users.noreply.github.com>
2023-03-20 16:23:04 +0300

    HW_task_22

--- HW_4/task_22.py
+++ HW_4/task_22.py
@@ -5,3 +5,27 @@
 # m - кол-во элементов второго множества.
 # Затем пользователь вводит сами элементы множеств.
 
+import random
+
+A = int(input("Введите A: "))  # кол-во элементов в 1-м массиве
+B = int(input("Введите B: "))  # кол-во элементов во 2-м массиве
+
+list_A = []
+list_B = []
+
+for i in range(A):
+    list_A.append(random.randint(-10, 10))
+
+for i in range(B):
+    list_B.append(random.randint(-10, 10))
+
+print(f'Массив_A: {list_A}')
+print(f'Массив_B: {list_B}')
+
+list_result = []
+
+for i in list_A:
+    for k in list_B:
+        if i == k and i not in list_result:
+            list_result.append(i)
+print(f"Искомый массив: {sorted(list_result)}")
