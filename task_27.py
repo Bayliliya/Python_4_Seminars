024375222c645578c23692dfb82d3a99f068d3f2
Liliya <105841191+Bayliliya@users.noreply.github.com>
2023-03-20 14:54:32 +0300

    '27

--- task_27.py
+++ task_27.py
@@ -8,3 +8,11 @@
 # shells on the sea shore I'm sure that the shells are sea
 # shore shells
 
+input_text = """She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"""
+
+input_text = input_text.split(' ')
+unique_word_text = set()
+
+for word in input_text:
+    unique_word_text.add(word.lower())
+print(unique_word_text, '\n', len(unique_word_text))
